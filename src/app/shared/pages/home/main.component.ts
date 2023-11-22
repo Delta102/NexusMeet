8d9df7b6f4286e6a209a042f3829ae307fb3b45a
Syntax <alfrefp10001@gmail.com>
2023-11-22 09:24:09 -0500

    Visual modifications v4

--- src/app/shared/pages/home/main.component.ts
+++ src/app/shared/pages/home/main.component.ts
@@ -12,7 +12,8 @@ export class MainComponent implements OnInit{
 
   constructor( private eventService: EventService){};
 
-  imageTwo = 'https://img.freepik.com/foto-gratis/salon-banquetes-decorado-mesa-redonda-servida-centro-mesa-hortensias-sillas-chiavari_8353-10059.jpg?w=996&t=st=1662586503~exp=1662587103~hmac=370e6bd8eb5009deede78fe2c488dcb326d8bc36f4ea119076470c47a9173916';
+  imageOne = 'https://img.freepik.com/fotos-premium/operador-camara-video-trabajando-su-equipo-evento-interior_44344-333.jpg?w=1060';
+  // imageTwo = 'https://img.freepik.com/foto-gratis/salon-banquetes-decorado-mesa-redonda-servida-centro-mesa-hortensias-sillas-chiavari_8353-10059.jpg?w=996&t=st=1662586503~exp=1662587103~hmac=370e6bd8eb5009deede78fe2c488dcb326d8bc36f4ea119076470c47a9173916';
   imageTree = 'https://img.freepik.com/fotos-premium/gente-feliz-baila-concierto-fiesta-discoteca_31965-6829.jpg?w=1060';
 
   ngOnInit(): void {
